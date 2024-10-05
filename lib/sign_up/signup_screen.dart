0d31323a86a6675f8b96f03c872a7b8d0b8b90c3
unknown <meroo1995saggaf@outlook.com>
2024-10-05 13:02:55 +0300

    add some files and update the project

---
+++ lib/sign_up/signup_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:pinterest_clone/forget_password/heading_text.dart';
+import 'package:pinterest_clone/forget_password/info.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.red, Colors.redAccent]),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HeadText(),
+                Credentials(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
